5f8324eb00987fe664a73d92af905ce8d28c8ba8
Lucy Fox <lucyreneefox@gmail.com>
2016-09-15 12:30:53 -0600

    Refactored hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,14 +1,7 @@
 class Hamming
-  def self.compute(string1, string2)
-    if string1.length == string2.length
-      compare_strings(string1, string2)
-    else
-      raise ArgumentError.new("Strings must be of equal length")
-    end
-  end
-
-  def self.compare_strings(string1, string2)
-    matched = string1.chars.zip(string2.chars)
+  def self.compute(str1, str2)
+    raise ArgumentError.new("Strings must be of equal length") if str1.length != str2.length
+    matched = str1.chars.zip(str2.chars)
     matched.keep_if {|letters| letters[0] != letters[1]}.count
   end
 end

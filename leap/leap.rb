843b02510b12c8668bda8f821c8593adede431f1
Lucy Fox <lucyreneefox@gmail.com>
2016-11-07 16:54:08 -0700

    Add leap solution

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,5 +1,10 @@
 class Year
-
-  def self.leap?
+  # on every year that is evenly divisible by 4
+  #   except every year that is evenly divisible by 100
+  #     unless the year is also evenly divisible by 400
+  def self.leap?(year)
+    if year % 4 == 0
+      year % 100 != 0 || year % 400 == 0
+    end
   end
 end

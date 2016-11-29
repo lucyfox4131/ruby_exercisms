2ad80c3f72916af50ab078acf3c703538b2744c1
Lucy Fox <lucyreneefox@gmail.com>
2016-11-28 17:50:23 -0700

    Add uniq to word counts

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,15 +1,25 @@
 class Phrase
   def initialize(phrase)
     @phrase = phrase
+    @counts = {}
   end
 
   def word_count
-    words = phrase.split(" ")
+    words = phrase.split(" ").uniq
+
+    words.map do |word|
+      counts[word] = 0
+    end
+    
   end
 
   private
 
   def phrase
     @phrase
+  end
+
+  def counts
+    @counts
   end
 end
